f0fe71380ef3fda9d2c8b2fddd2e885828cc26bb
shakhawat.dev <shakhawat.code@gmail.com>
2025-11-06 15:05:39 +0600

    user-auth fixed

--- backend/middleweres/authMiddlewere.js
+++ backend/middleweres/authMiddlewere.js
@@ -21,8 +21,7 @@ async function authMiddlewere(req , res , next) {
         // find user in database
         const user = await userSchema.findOne({ refreshToken : cookie })
 
-        if(!user) return res.status(400).send({ success: false , message : "Authentication failed or user not found " })  // user not found
-
+ 
 
         // verify token 
         jwt.verify(token , process.env.ACCESS_TOKEN_SECRET , (error , decodedToken)=>{ 
